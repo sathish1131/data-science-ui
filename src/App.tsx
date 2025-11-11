f106dfe648de3819f3b94b9109e207d3e28e158f
sathish <sathishkumar1131@gmail.com>
2025-11-12 00:29:09 +0530

    react typescript project done

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-blue-500 text-4xl font-bold">Hello Bae 💙</h1>
+    </div>
+  )
+}
+
+export default App;
